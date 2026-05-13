2a3a3108221ba33d0a3b8027b516511f26872405
M-NASCAR <mazen.nassar@education.nsw.gov.au>
2026-05-13 23:13:04 +1000

    Started Work on Function #1
    I started to work on the first function which allows users to identify specific suburb median prices in houses, and add more into the documentation file

--- PROJECT_DOCUMENTATION.md
+++ PROJECT_DOCUMENTATION.md
@@ -1,4 +1,12 @@
 # Assesment Task 1
 
 #### *Hypothesis:*
-- The 
+- My Hypothesis is that the closer you are to the Sydney CBD the higher the price of houses are
+
+#### *Requirements:*
+> - Data Loading - This project should have the ability to load files and handle errors such as missing files
+> - Data Cleaning - This project should allow users to filter, remove and add entries to the data
+> - Visulisation - This project allows for visulisation of data as graphs using matplotlib and viewing dataframes using pandas
+> - Reporting - Updating data and allowing users to save
+> The goal is to allow users to access,update and filter existing data through the user interface
+
--- data_module.py
+++ data_module.py
@@ -3,54 +3,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 df = pd.read_csv("domain_properties.csv")
+pd.set_option('display.float_format', '{:.2f}'.format)
 
-datetypes = pd.DatetimeIndex(df["date_sold"])
-df["Year"] = datetypes.year
-
-
-def display_df_preview():  #Function to Preview the Dataset
-    print(df)
-display_df_preview
 
 suburb_df = df.groupby('suburb')['price'].median().reset_index()
 suburb_df.columns = ['suburb', 'median_price']
-suburb_df = suburb_df.sort_values('median_price', ascending=False)
-print(suburb_df)
+
+#df["Mean_price"] = df["suburb"].map(suburb_df)
+
+def display_df_preview():  #Function to Preview the Dataset
+    print("1. Preview Dataset")
+    print("2.Search for the median price of houses")
+    print("3.Exit")
+    choice = input("Select an option (1-3): ").strip()
+    while choice != "3":
+        if choice == "1":
+            print(df)
+           
+
+        elif choice == "2":
+            userinput = input("Enter the suburb you wish to know the mean price of: ").strip().title()
+            result = suburb_df[suburb_df['suburb'] == userinput]
+            if result.empty:
+                print("Suburb Not found, Try again.")
+            else:
+                print(result)
+display_df_preview()
 
 
+
+'''
 suburb_df.plot(
-    kind="bar",
-    x="suburb",
-    y="median_price",
+ kind="bar",
+ x="suburb",
+ y="mean_price",
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-df.plot(kind="bar",
-          x="num_parking",
-          y="price",
-          alpha=0.5
-          )
 '''
 
 
-plt.show()
+
+
+
+
+
+#plt.show()
 
 
