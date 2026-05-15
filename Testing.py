85c6b2ea3b9910ab2b5267bf181d7a85b3312f6e
M-NASCAR <mazen.nassar@education.nsw.gov.au>
2026-05-15 12:31:04 +1000

    Continued work on the search function

--- Testing.py
+++ Testing.py
@@ -2,3 +2,27 @@ import pandas as pd
 
 df = pd.read_csv("domain_properties.csv", on_bad_lines='skip')
 pd.set_option('display.float_format', '{:.2f}'.format)
+
+def display_df_preview(): 
+    preview_menu() #Function to Preview the Dataset
+    choice = int(input("Select an option (1-3): ").strip())
+    while choice != 3:
+        if choice == 1:
+            print(df)
+            preview_menu()
+            choice = int(input("Select an option (1-3): ").strip())
+           
+
+        elif choice == 2:
+            userinput = input("Enter the suburb that you wish to know median house prices in: ").strip().title()
+            result = suburb_df[suburb_df['suburb'] == userinput][['suburb', 'median_price','km_from_cbd']].drop_duplicates(subset='suburb')
+            #result = df[df[['suburb'] == userinput][['suburb', 'median_price']]]
+            if result.empty:
+                print("Suburb Not found, Try again.")
+            else:
+                print(result)
+
+        elif choice == 3:
+            main_menu()
+            
+
--- data_module.py
+++ data_module.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,26 +10,31 @@ pd.set_option('display.float_format', '{:.2f}'.format)
 
 #df['median_price'] = ['median_price']
 #print(df)
-
 suburb_df = df.groupby('suburb')['price'].median().reset_index()
 suburb_df.columns = ['suburb', 'median_price']
 suburb_df['km_from_cbd'] = suburb_df['suburb'].map(df.groupby('suburb')['km_from_cbd'].first())
+
 #suburb_df["km_from_cbd"] = df["km_from_cbd"]
 
 #print(suburb_df)
 #df["Mean_price"] = df["suburb"].map(suburb_df
 
+def clear_terminal():
+     os.system('cls')
+
+
+
 def preview_menu(): #Function for the Menu inside the data preview section
     print("1. Preview Dataset")
     print("2.Search for details on a specific suburb:")
     print("3.Exit")
+    
 
 
-
-def display_df_preview(): 
-    preview_menu() #Function to Preview the Dataset
-    choice = int(input("Select an option (1-3): ").strip())
-    while choice != 3:
+def display_df_preview():
+    while True:
+        preview_menu() #Function to Preview the Dataset
+        choice = int(input("Select an option (1-3): ").strip())
         if choice == 1:
             print(df)
             preview_menu()
@@ -38,15 +44,28 @@ def display_df_preview():
         elif choice == 2:
             userinput = input("Enter the suburb that you wish to know median house prices in: ").strip().title()
             result = suburb_df[suburb_df['suburb'] == userinput][['suburb', 'median_price','km_from_cbd']].drop_duplicates(subset='suburb')
-            #result = df[df[['suburb'] == userinput][['suburb', 'median_price']]]
             if result.empty:
                 print("Suburb Not found, Try again.")
+                print("2.Search for details on a specific suburb:")
+                print("3.Exit")
+                choice = int(input("Select an option (1-3): ").strip())
+
             else:
                 print(result)
+                print("2.Search for details on a specific suburb:")
+                print("3.Exit")
+                choice = int(input("Select an option (2-3): ").strip())
+
+
 
         elif choice == 3:
-            main_menu()
-            
+                clear_terminal()
+                main_menu()
+                print(67)
+                break
+        else:
+                print("Not a valid option please try again")
+         
 
 
 
@@ -64,20 +83,21 @@ def main_menu():  #Function for the main menu
         print("5. Save changes")
         print("6. Exit")
 
-        choice = input("Select an option (1-6): ").strip()
+        main_choice = input("Select an option (1-6): ").strip()
 
-        if choice == '1':
+        if main_choice == '1':
+            clear_terminal()
             display_df_preview()
-        elif choice == '2':
+        elif main_choice == '2':
             display_visualisation()
-        elif choice == '3':
+        elif main_choice == '3':
             search_data()
-        elif choice == '4':
+        elif main_choice == '4':
             update_data_entry()
-        elif choice == '5':
+        elif main_choice == '5':
             save_changes()
             print("Changes saved.")
-        elif choice == '6':
+        elif main_choice == '6':
             print("Exiting program.")
             break
         else:
